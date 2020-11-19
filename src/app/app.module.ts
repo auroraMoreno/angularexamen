8e4f99e6893359172f1e63215d3ec0ee0197055c
Aurora Moreno Lopez <aurora.moreno.lopez@tajamar365.com>
2020-11-19 19:10:39 +0100

    bootstrap,forms,routing,carpetas

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {routing, appRoutingProviders} from './app.routing';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, routing, FormsModule
+  ],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import {Routes, RouterModule} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+
+const appRoutes: Routes=[]
+
+export const appRoutingProviders : any[] = [];
+export const routing: ModuleWithProviders<any> = 
+RouterModule.forRoot(appRoutes);
